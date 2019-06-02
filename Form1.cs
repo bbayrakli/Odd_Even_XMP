90a3d76bc860a1048a522d277396f04f944de738
bbayrakli <bayrakliburak@outlook.com>
2019-06-02 16:05:35 +0300

    added

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odd_Even_XMP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int sayi;
+
+            sayi = Convert.ToInt16(textBox1.Text);
+
+            if (sayi % 2 == 0)
+            {
+                label1.Text = (sayi.ToString() + "  is an even number");
+            }
+            else
+            {
+                label1.Text = (sayi.ToString() + "  is an odd number");
+            }
+           
+                   
+        }
+    }
+}
